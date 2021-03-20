3415dd7408750eae01321acad1ca00afcfd368eb
Srikar <srikarg89@gmail.com>
2021-03-20 11:51:23 -0400

    Remove ValveArduino code and rewrite launchbox code to handle pulsing and special

--- constants.h
+++ constants.h
@@ -5,46 +5,47 @@ pulse - you push it and it opens, waits .05s, and closes
 Nitrogen Fill (1 switch (toggle)))
 Ethanol Drain (1 switch (toggle))
 Ethanol Vent (1 button (pulse), 1 switch (toggle))
-Ethanol Main Propellant TEENSY (1 switch (toggle))
+Ethanol Main Propellant Valve (1 switch (toggle))
 
 Nitrous Oxide Fill
 Nitrous Oxide Drain
 Nitrous Oxide Vent
-Nitrous Oxide Main Propellant TEENSY
+Nitrous Oxide Main Propellant Valve
 */
 
-#include "Arduino.h"
-
 // Pin definitions - each toggle switch takes up 2 pins, the pulse pins take up one pin each
-// On the launchbox
+
+// Nitrous valves
 #define NO_FILL 0
+#define NO_FILL_SPECIAL false
+#define NO_FILL_IS_NC false
 #define NO_DRAIN 2
+#define NO_DRAIN_SPECIAL false
+#define NO_DRAIN_IS_NC false
 #define NO_VENT 4
+#define NO_VENT_SPECIAL false
+#define NO_VENT_IS_NC false
 #define NO_MPV 6
+#define NO_MPV_SPECIAL false
+#define NO_MPV_IS_NC false
 
-
+// Ethanol valves
 #define NITROGEN_FILL 8
+#define NITROGEN_FILL_SPECIAL false
+#define NITROGEN_FILL_IS_NC false
 #define ETHANOL_DRAIN 10
+#define ETHANOL_DRAIN_SPECIAL false
+#define ETHANOL_DRAIN_IS_NC false
 #define ETHANOL_VENT 12
+#define ETHANOL_VENT_SPECIAL false
+#define ETHANOL_VENT_IS_NC false
 #define ETHANOL_MPV 14
-#define NO_FILL 4
-#define NO_DRAIN 6
-#define NO_VENT 8
-#define NO_MPV 10
+#define ETHANOL_MPV_SPECIAL false
+#define ETHANOL_MPV_IS_NC false
+
+// Pulsing buttons
+#define NO_VENT_PULSE 19
+#define NO_VENT_PULSE_SPECIAL false
 #define ETHANOL_VENT_PULSE 20
-#define NO_VENT_PULSE A19
-#define ABORT_PIN A20
-
-// TEENSY arduino pin definitions - MAKE SURE THESE MATCH TEENSY ARDUINO
-
-#define TEENSY_NITROGEN_FILL 2
-#define TEENSY_ETHANOL_DRAIN 3
-#define TEENSY_ETHANOL_VENT 4
-#define TEENSY_ETHANOL_MPV 5
-#define TEENSY_NO_FILL 6
-#define TEENSY_NO_DRAIN 7
-#define TEENSY_NO_VENT 8
-#define TEENSY_NO_MPV 9
-
-// Pin order and actuation types
-#define PULSE 4
+#define ETHANOL_VENT_PULSE_SPECIAL false
+#define ABORT_PIN 20
--- launch-box.ino
+++ launch-box.ino
@@ -14,106 +14,175 @@ Nitrous Oxide Main Propellant Valve
 */
 
 #include "constants.h"
-#define HWSERIAL Serial5
 
-enum PinState {
+#define PULSE_TIME 500
+#define SPECIAL_OPEN_TIME 4000
+#define SPECIAL_CLOSE_TIME 1000
+
+// default is 2
+enum pin_state {
     DO_NOTHING = 1,
     OPEN_VENT = 2,
     CLOSE_VENT = 3
 };
 
-const int BAUD_RATE = 9600;
-const int MAX_PIN = 25;
-
-// PIN_MAP[valve pin] = [teensy pin]
-int PIN_MAP[MAX_PIN];
+// Pin counts
+const int NUM_VALVES = 8;
+const int NUM_BUTTONS = 2;
 
 // Local variables
-boolean aborted = false;
-
-// Number of valves, equal to the length of ventPins[].
-const int NUM_VALVES = 8;
-int ventPins[] = {NITROGEN_FILL, ETHANOL_DRAIN, ETHANOL_VENT, ETHANOL_MPV, NO_FILL, NO_DRAIN, NO_VENT, NO_MPV};
-
-// Number of buttons, less than or equal to the length of pulsePins[].
-const int NUM_BUTTONS = 0;
-int pulsePins[] = {ETHANOL_VENT_PULSE, NO_VENT_PULSE};
+boolean aborted;
 
 // Arrays that keep track of stuff
 // States: 0 means do nothing, 1 is CLOSE_VENT, and 2 is OPEN_VENT (matches the constants)
-PinState valveStates[NUM_VALVES];
-boolean pulsing[NUM_BUTTONS];
+pin_state states[NUM_VALVES];
+unsigned long pulse_timings[NUM_VALVES];
+unsigned long special_open_timings[NUM_VALVES];
+boolean special_close_timings[NUM_VALVES];
+int vent_pins[] = {NITROGEN_FILL, ETHANOL_DRAIN, ETHANOL_VENT, ETHANOL_MPV, NO_FILL, NO_DRAIN, NO_VENT, NO_MPV};
+boolean special_valves[] = {NITROGEN_FILL_SPECIAL, ETHANOL_DRAIN_SPECIAL, ETHANOL_VENT_SPECIAL, ETHANOL_MPV_SPECIAL, NO_FILL_SPECIAL, NO_DRAIN_SPECIAL, NO_VENT_SPECIAL, NO_MPV_SPECIAL};
+boolean nc_valves[] = {NITROGEN_FILL_IS_NC, ETHANOL_DRAIN_IS_NC, ETHANOL_VENT_IS_NC, ETHANOL_MPV_IS_NC, NO_FILL_IS_NC, NO_DRAIN_IS_NC, NO_VENT_IS_NC, NO_MPV_IS_NC};
+int pulse_pins[] = {-1, -1, ETHANOL_VENT_PULSE, -1, -1, -1, NO_VENT_PULSE, -1};
 
-void setup() {
-    // These ten pins control everything
-    PIN_MAP[NITROGEN_FILL] = TEENSY_NITROGEN_FILL;
-    PIN_MAP[ETHANOL_DRAIN] = TEENSY_ETHANOL_DRAIN;
-    PIN_MAP[ETHANOL_VENT] = TEENSY_ETHANOL_VENT;
-    PIN_MAP[ETHANOL_VENT_PULSE] = TEENSY_ETHANOL_VENT;
-    PIN_MAP[ETHANOL_MPV] = TEENSY_ETHANOL_MPV;
-    PIN_MAP[NO_FILL] = TEENSY_NO_FILL;
-    PIN_MAP[NO_DRAIN] = TEENSY_NO_DRAIN;
-    PIN_MAP[NO_VENT] = TEENSY_NO_VENT;
-    PIN_MAP[NO_VENT_PULSE] = TEENSY_NO_VENT;
-    PIN_MAP[NO_MPV] = TEENSY_NO_MPV;
-
-    for (int i = 0; i < NUM_VALVES; i++){
-        pinMode(ventPins[i], INPUT_PULLUP);
-        pinMode(ventPins[i] + 1, INPUT_PULLUP);
+void setup(){
+    for(int i = 0; i < NUM_VALVES; i++){
+        pinMode(vent_pins[i], INPUT_PULLUP);
+        pinMode(vent_pins[i] + 1, INPUT_PULLUP);
     }
-    
-    for (int i = 0; i < NUM_BUTTONS; i++){
-        pinMode(pulsePins[i], INPUT_PULLUP);
+    for(int i = 0; i < NUM_BUTTONS; i++){
+        pinMode(pulse_pins[i], INPUT_PULLUP);
     }
-    
     pinMode(ABORT_PIN, INPUT_PULLUP);
-    Serial.begin(BAUD_RATE);
+    Serial.begin(9600);
+    aborted = false;
     
-    for (int i = 0; i < NUM_VALVES; i++){
-        valveStates[i] = DO_NOTHING; 
-    }
-    
-    for (int i = 0; i < NUM_BUTTONS; i++){
-        pulsing[i] = false;
+    for(int i = 0; i < NUM_VALVES; i++){
+        states[i] = DO_NOTHING;
+        pulse_timings[i] = 0;
+        special_open_timings[i] = 0;
+        special_close_timings[i] = 0;
     }
 }
 
-void loop() {
-    // For each valve in a different state from the switch state, actuate it.
-    for (int i = 0; i < NUM_VALVES; i++) {
-        int ventPin = ventPins[i];
-        int launchboxPin = PIN_MAP[ventPin];
-
-        PinState launchboxSwitchState = readLaunchboxSwitch(launchboxPin);
-        if (launchboxSwitchState != valveStates[i]) {
-            sendMessage(launchboxSwitchState, ventPin);
-            valveStates[i] = launchboxSwitchState;
-
-            Serial.println(String("State of toggle switch at LB pin ") + ventPin + ": " + launchboxSwitchState);
+void loop(){
+    for(int i = 0; i < NUM_VALVES; i++){
+        int pin = vent_pins[i];
+        pin_state current_state = checkToggleSwitch(pin); // (i + 1) * 2 maps from array index to pin number
+        if(current_state != states[i]){
+            states[i] = current_state;
+            if(states[i] == OPEN_VENT && isSpecial(i)){
+              special_open_timings[i] = millis() + SPECIAL_OPEN_TIME;
+            }
+        }
+        if(pulse_pins[i] != -1){
+          boolean pressed = buttonRead(pulse_pins[i]);
+          if(pressed){
+            pulse_timings[i] = millis() + PULSE_TIME;
+            digitalWrite(pulse_pins[i], HIGH);
+          }
+        }
+        if(isPulsing(i)){
+          handlePulse(i);
+        }
+        else{
+          handleVent(i);
+          handleSpecial(i);
         }
     }
-
-    delay(50);
 }
 
-/**
- * This method measures a button value, waits 20ms, and measures the button value again.
- */
-int measureAnalogDebounced(int pin) {
-    int first = analogRead(pin) > 900;
+void handleVent(int index){
+  int pin = vent_pins[index];
+  int open_signal = HIGH;
+  int close_signal = LOW;
+  if(!isNC(index)){
+    open_signal = LOW;
+    close_signal = HIGH;
+  }
+  if(states[index] == OPEN_VENT){
+    // If it's a special, check if its currently holding open for 4s or reliving for 1s
+    if(isSpecial(index)){
+      if(special_open_timings[index] != 0){
+        digitalWrite(pin, open_signal);
+      }
+      else{
+        digitalWrite(pin, close_signal);
+      }
+    }
+    // If it's not a special valve, then digitalWrite HIGH on open
+    else{
+      digitalWrite(pin, open_signal);
+    }
+  }
+  // If the valve is closing, then digitalWrite LOW
+  else if(states[index] == CLOSE_VENT){
+    digitalWrite(pin, close_signal);
+  }
+}
+
+void handleSpecial(int index){
+  if(!special_valves[index]){
+    return;
+  }
+  if(states[index] != OPEN_VENT){
+    return;
+  }
+  unsigned long curr = millis();
+  if(special_open_timings[index] != 0 && curr > special_open_timings[index]){
+    special_open_timings[index] = 0;
+    special_close_timings[index] = curr + SPECIAL_CLOSE_TIME;
+  }
+  else if(special_close_timings[index] != 0 && curr > special_close_timings[index]){
+    special_close_timings[index] = 0;
+    special_open_timings[index] = curr + SPECIAL_OPEN_TIME;    
+  }
+}
+
+void handlePulse(int index){
+  // Ignore this method if the valve isn't pulsing
+  if(!isPulsing(index)){
+    return;
+  }
+
+  // If it's finished pulsing (it's been open for 500ms), then make it stop pulsing. Otherwise, keep pulsing.
+  unsigned long curr = millis();
+  if(curr > pulse_timings[index]){
+    pulse_timings[index] = 0;
+    digitalWrite(pulse_pins[index], LOW);
+  }
+  else{
+    digitalWrite(pulse_pins[index], HIGH);
+  }
+}
+
+boolean isNC(int index){
+  return nc_valves[index];
+}
+
+boolean isSpecial(int index){
+  return special_valves[index];
+}
+
+boolean canPulse(int index){
+  return pulse_pins[index] != 0;
+}
+
+// Check if a given valve is currently pulsing
+boolean isPulsing(int index){
+  return pulse_timings[index] != 0;
+}
+
+// Returns true if a given button is being pressed, false if not.
+int buttonRead(int pin){
+    int first = (analogRead(pin) > 900);
     delay(20);
-    int second = analogRead(pin) > 900;
-
-    return first && second;
+    int second = (analogRead(pin) > 900);
+    if(first == second) {
+      return first;  
+    }
+    return false;
 }
 
-/**
- * Writes a message to a given pin.
- */
-void sendMessage(int cmd, int pin) {
-    Serial.println(String("Sending command ") + cmd + String(" to TEENSY Arduino pin ") + pin);
-    digitalWrite(pin, cmd);
-}
 
 /*
  * note: these SPDT switches all have their common pins connected to GND because arduino only has an INPUT_PULLUP option instead of an INPUT_PULLDOWN option
@@ -139,18 +208,14 @@ void sendMessage(int cmd, int pin) {
  * because of how SPDT works, the opposite pin is LOW
 */
 
-PinState readLaunchboxSwitch(int switchStart) {
-  // Pinstate is controlled by two consecutive pins.
-  // If the first pin is HIGH, then we want to open the vent.
-  // This looks wrong but I want to double check with someone.
-
-  if (digitalRead(switchStart) == LOW) {
+pin_state checkToggleSwitch(int switchStart) {
+  if(digitalRead(switchStart) == LOW) {
     return OPEN_VENT;
+  }  
+  else if(digitalRead(switchStart + 1) == LOW) {
+    return CLOSE_VENT;
   }
-  
-  if (digitalRead(switchStart + 1) == LOW) {
-    return CLOSE_VENT;   
+  else {
+    return DO_NOTHING;
   }
-  
-  return DO_NOTHING;
 }

fb4410869357274cc40f6f57856c6874457981c4
Eric W. Feng <65430860+ericwfeng@users.noreply.github.com>
2021-09-12 17:09:39 -0400

    updated constants

--- constants.h
+++ constants.h
@@ -20,24 +20,35 @@ Nitrous Oxide Main Propellant Valve
 #define NO_FILL 0
 #define NO_FILL_SPECIAL true
 #define NO_FILL_IS_NC true
-#define NO_VENT 2
+#define NO_VENT 2 //switch 1
 #define NO_VENT_SPECIAL false // only false
 #define NO_VENT_IS_NC false // only false
-#define NO_MPV 4
+#define NO_MPV 4 //switch 2
 #define NO_MPV_SPECIAL true
 #define NO_MPV_IS_NC true
-#define NO_DRAIN 6
+#define NO_DRAIN 6 //switch 3
 #define NO_DRAIN_SPECIAL true
 #define NO_DRAIN_IS_NC true
-
+#define es1 6//switch 4
+#define es1_SPECIAL true
+#define es1_IS_NC true
+#define es2 8//switch 5
+#define es2_SPECIAL true
+#define es2_IS_NC true
+#define es3 10//switch 6
+#define es3_SPECIAL true
+#define es3_IS_NC true
+#define es4 10//switch 7
+#define es4_SPECIAL true
+#define es4_IS_NC true
 // Ethanol valves
-#define NITROGEN_FILL 31
+#define NITROGEN_FILL 31 //switch 11
 #define NITROGEN_FILL_SPECIAL true
 #define NITROGEN_FILL_IS_NC true
-#define ETHANOL_VENT 29
+#define ETHANOL_VENT 29 //switch 10
 #define ETHANOL_VENT_SPECIAL true// test special
 #define ETHANOL_VENT_IS_NC true 
-#define ETHANOL_MPV 27
+#define ETHANOL_MPV 27 //switch 9
 #define ETHANOL_MPV_SPECIAL true
 #define ETHANOL_MPV_IS_NC true
 //#define ETHANOL_DRAIN 25
@@ -53,7 +64,6 @@ Nitrous Oxide Main Propellant Valve
 #define ETHANOL_VENT_PULSE_SPECIAL true
 #define ABORT_PIN 37
 #define AUTOSEQ_PIN -1
-//#define IGNITER_PIN A21
 // Output pins
 
 // Nitrous valves
@@ -61,12 +71,14 @@ Nitrous Oxide Main Propellant Valve
 #define NO_DRAIN_OUT 23
 #define NO_VENT_OUT 20
 #define NO_MPV_OUT 21
-
+#define es1_OUT 16
+#define es2_OUT 17
+#define es3_OUT 19
+#define es4_OUT 13
 // Ethanol valves
 #define NITROGEN_FILL_OUT 15
 #define ETHANOL_DRAIN_OUT 22
 #define ETHANOL_VENT_OUT 14
 #define ETHANOL_MPV_OUT 19 // 23
-
 // extra
 #define IGNITER_OUT 17
